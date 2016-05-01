ab787b0d0ec81207d7b508ec7f9946b6087d174d
vahid <vahid.mardani@gmail.com>
2016-05-01 19:26:04 +0430

    single file handler

--- src/kakapo/http/controllers/__init__.py
+++ src/kakapo/http/controllers/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from .jwt import JwtPrincipalController
-from .static import SingleFileHandler
+from .static import SingleFileHandler, single_file_handler
 __author__ = 'vahid'
