e82f4f3bf511c6890ca29d327f74e6e1990b9553
Shahab Rezaee <shahabrezaee@gmail.com>
2016-09-21 18:44:22 +0330

    fix context without stack -- not ready for use

--- src/kakapo/http/context.py
+++ src/kakapo/http/context.py
@@ -5,22 +5,54 @@ __author__ = 'vahid'
 _thread_local = local()
 
 
-class Context(dict):
-
-    def __init__(self, *args, **kw):
-        super(Context, self).__init__(*args, **kw)
-        self.update({'__id': get_ident()})
+class Context(object):
+    def __init__(self, *args, **kwargs):
+        global _thread_local
+        if hasattr(_thread_local, 'store'):
+            raise AlreadyInitializedError('Context is already initialized')
+        _thread_local.store = dict(*args, **kwargs)
 
     @classmethod
     def current(cls):
-        if not hasattr(_thread_local, 'kakapo_context'):
-            _thread_local.kakapo_context = cls()
-        return _thread_local.kakapo_context
+        return cls()
 
     @staticmethod
     def destroy():
-        if hasattr(_thread_local, 'kakapo_context'):
-            delattr(_thread_local, 'kakapo_context')
+        global _thread_local
+        if hasattr(_thread_local, 'store'):
+            delattr(_thread_local, 'store')
+
+    def update(self, *args, **kwargs):
+        global _thread_local
+        if not hasattr(_thread_local, 'store'):
+            raise ValueError('store not created yet')
+        _thread_local.store.update(*args, **kwargs)
+
+    @staticmethod
+    def get(*args, **kwargs):
+        global _thread_local
+        return _thread_local.store.get(*args, **kwargs)
+
+    @staticmethod
+    def set(*args, **kwargs):
+        global _thread_local
+        _thread_local.store.set(*args, **kwargs)
+
+    def __getitem__(self, key):
+        global _thread_local
+        return _thread_local.store[key]
+
+    def __setitem__(self, key, value):
+        global _thread_local
+        _thread_local.store[key] = value
+
+    def __delitem__(self, key):
+        global _thread_local
+        del _thread_local.store[key]
+
+    def keys(self):
+        global _thread_local
+        return _thread_local.store.keys()
 
 
 class SingletonPerContext(type):
@@ -28,6 +60,10 @@ class SingletonPerContext(type):
     def __call__(cls, *args, **kwargs):
         context_key = '%s' % cls.__name__
         context = Context.current()
-        if context_key not in context:
+        if context_key not in context.keys():
             context[context_key] = super(SingletonPerContext, cls).__call__(*args, **kwargs)
         return context[context_key]
+
+
+class AlreadyInitializedError(Exception):
+    pass
